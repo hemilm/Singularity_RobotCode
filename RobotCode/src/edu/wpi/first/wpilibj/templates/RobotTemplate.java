9d94dbced00468f5f6b1a4dc54d3c73ee8384c19
Mistry <mistryh999@gmail.com>
2014-02-02 18:17:14 -0500

    1.8.1

--- RobotCode/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ RobotCode/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -15,6 +15,16 @@ import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer; 
 
+// used to read and write values into the robot code with out recompiling,
+//download available at https://code.google.com/p/json-simple/downloads/list
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -38,6 +48,43 @@ public class RobotTemplate extends SimpleRobot {
     
     double dir;
     
+    public class JsonSimpleExample {
+     public void readFile (String[] args) {
+ 
+	JSONParser parser = new JSONParser();
+ 
+	try {
+ 
+		Object obj = parser.parse(new FileReader("c:\\test.json"));
+ 
+		JSONObject jsonObject = (JSONObject) obj;
+ 
+		String name = (String) jsonObject.get("name");
+		System.out.println(name);
+                lcd.println(Line.kUser6, 1,name );
+                
+		String age = (String) jsonObject.get("age");
+		System.out.println(age);
+                lcd.println(Line.kUser6, 1,age );
+                
+		// loop array
+		//JSONArray msg = (JSONArray) jsonObject.get("messages");
+		//Iterator<String> iterator = msg.iterator();
+		//while (iterator.hasNext()) {
+		//	System.out.println(iterator.next());
+		//}
+ 
+	} catch (FileNotFoundException e)  {
+		e.printStackTrace();
+	} catch (IOException e) {
+		e.printStackTrace();
+        } catch (ParseException e) {
+		e.printStackTrace();
+	}
+ 
+     }
+ 
+}
     
     public void robotInit() {
         lcd.println(Line.kUser6, 1, "Robot Initialized");
